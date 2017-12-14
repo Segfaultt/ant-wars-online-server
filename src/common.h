5c858893fa249a2cba54307baa623c0536423aa9
ShrimpKeeper <xophnex@gmail.com>
2017-12-14 09:59:28 +0800

    Cleaned common.h and added unfinished map

--- src/common.h
+++ src/common.h
@@ -10,12 +10,6 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-
-const unsigned int screen_width = 1366, screen_height = 768;
-
 enum faction {
 	GERMANIC,
 	ROMAN,
@@ -24,18 +18,11 @@ enum faction {
 };
 
 const std::string FONT[] = {"res/Autobahn.ttf", "res/Roman SD.ttf", "res/Red October.ttf"},
-	NAME[] = {"Das Reich", "SPQR", "Soviet Socialist Union"};
+	NAME[] = {"German Reich", "SPQR", "Soviet Socialist Union"};
 
-const SDL_Color COLOR_TITLE[] = {{0x0, 0x0, 0x0}, {0xFF, 0xF1, 0x0}, {0xFF, 0xF1, 0x0}},
-	COLOR_FG[] = {{0xDB, 0x01, 0x00}, {0xFF, 0xF1, 0x0}, {0xFB, 0xF1, 0xF0}},
-	COLOR_BG[] = {{0xFD, 0xCD, 0x01}, {0xFD, 0x0B, 0x14}, {0xED, 0x1B, 0x24}};
-
-const int SIZE_TITLE[] = {80, 40, 44},
-	SIZE_FG[] = {50, 24, 24};
-
-const Uint16 PORT_CLIENT = 52757,
+const int PORT_CLIENT = 52757,
 	PORT_SERVER = 52657;
 
 const unsigned int BUFFER_SIZE = 512;
 
-const std::string VERSION = "c0.0";
+const std::string VERSION = "s0.0";
